c8e8d5040c58c7d55f581e8b291a84bd376564c8
wangjunchen <wangjunchen2017@outlook.com>
2022-12-04 20:38:28 +0800

    feat 创建了SpringCloud项目，开始SpringCloud的学习。 先创建单体应用

---
+++ SpringCloud-01-single/src/main/java/com/powernode/SpringCloud01SingleApplication.java
@@ -0,0 +1,13 @@
+package com.powernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloud01SingleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloud01SingleApplication.class, args);
+    }
+
+}
